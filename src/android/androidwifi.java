de8896af6b372c72b80dc67473a5cd5ee70e0b05
Jeroen <j.vanderlaarse@esperantoxl.nl>
2020-03-08 15:42:08 +0100

    A

--- src/android/androidwifi.java
+++ src/android/androidwifi.java
@@ -52,6 +52,7 @@ public class AndroidWifi extends CordovaPlugin {
     // Store AP, previous, and desired wifi info
     private AP previous, desired;
 
+    private final BroadcastReceiver networkChangedReceiver = new NetworkChangedReceiver();
     private static final IntentFilter NETWORK_STATE_CHANGED_FILTER = new IntentFilter();
 
     static {
@@ -437,6 +438,43 @@ public class AndroidWifi extends CordovaPlugin {
         }
         return false;
     }
+
+    /**
+   * Network Changed Broadcast Receiver
+   */
+  private class NetworkChangedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+
+      if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())) {
+
+        Log.d(TAG, "NETWORK_STATE_CHANGED_ACTION");
+
+        NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+        WifiInfo info = WifiWizard2.this.wifiManager.getConnectionInfo();
+
+        // Checks that you're connected to the desired network
+        if (networkInfo.isConnected() && info.getNetworkId() > -1) {
+
+          final String ssid = info.getSSID().replaceAll("\"", "");
+          final String bssid = info.getBSSID();
+
+          Log.d(TAG, "Connected to '" + ssid + "' @ " + bssid);
+
+          // Verify the desired network ID is what we actually connected to
+          if ( desired != null && info.getNetworkId() == desired.apId ) {
+            onSuccessfulConnection();
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
 
       /**
    * Register Receiver for Network Changed to handle BindALL
